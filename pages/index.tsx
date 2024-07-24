410b8c3ad7764d645ec62d0d2f826c01de90a454
Sergey Korshikov <sergey-korshikov@bk.ru>
2024-07-24 17:28:27 +0300

    init

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,9 @@ import styles from "@/styles/Home.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+export default function Home(): JSX.Element {
+  const a = 1;
+
   return (
     <>
       <Head>
@@ -104,7 +106,7 @@ export default function Home() {
             </h2>
             <p>
               Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
+              with&nbsp;Vercel. {a}
             </p>
           </a>
         </div>
